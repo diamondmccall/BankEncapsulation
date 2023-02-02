53d5b5a01a3f2ac4c0f5f7904293a5ba98f577a9
Diamond Mccall <singlikediamond@gmail.com>
2023-02-02 00:06:25 -0600

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAccount();
+            Console.WriteLine($"How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            myAccount.Deposit(amountToDeposit);
+           
+
+            Console.WriteLine($"Thank you! Your balance is now {myAccount.Getbalance()}");
+           
+            
+
         }
     }
 }
